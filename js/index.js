e36c4ade9d6c02d82ea80a924ed748f0dd1dcb39
Sebazt <juan.londono29@udea.edu.co>
2022-04-15 20:47:40 -0500

    antes de hacer el renderizado dinamico

--- js/index.js
+++ js/index.js
@@ -153,3 +153,8 @@ const sr = ScrollReveal({
 sr.reveal(`.home__data`)
 sr.reveal(`.home__handle`, {delay:700})
 sr.reveal(`.home__social, .home__scroll`, { delay: 900, origin: 'bottom' })
+
+
+/* ================ array SKIll ========================== */
+
+
---
+++ js/skills.js
@@ -0,0 +1,74 @@
+var skills =
+
+[
+  {
+    "id": 1,
+    "image": "../assets/html.png",
+    "name": "Html5"
+  },
+  {
+    "id": 2,
+    "image": "../assets/css.png",
+    "name": "Css3"
+  },
+  {
+    "id": 3,
+    "image": "../assets/js.png",
+    "name": "JavaScript"
+  },
+  {
+    "id": 4,
+    "image": "../assets/bootstrap.png",
+    "name": "Bootstrap"
+  },
+  {
+    "id": 5,
+    "image": "../assets/git.png",
+    "name": "Git"
+  },
+  {
+    "id": 6,
+    "image": "../assets/github.png",
+    "name": "Git-hub"
+  },
+  {
+    "id": 7,
+    "image": "../assets/react.png",
+    "name": "React"
+  },
+  {
+    "id": 8,
+    "image": "../assets/node.png",
+    "name": "Node.js"
+  },
+  {
+    "id": 9,
+    "image": "../assets/mongodb.png",
+    "name": "MongoDB"
+  },
+  {
+    "id": 10,
+    "image": "../assets/express.png",
+    "name": "Express"
+  },
+  {
+    "id": 11,
+    "image": "../assets/redux.png",
+    "name": "Redux"
+  },
+  {
+    "id": 12,
+    "image": "../assets/reactNative.png",
+    "name": "React-Native"
+  },
+  {
+    "id": 13,
+    "image": "../assets/postman.png",
+    "name": "Postman"
+  },
+  {
+    "id": 14,
+    "image": "../assets/insomia.png",
+    "name": "Insomia"
+  }
+]
