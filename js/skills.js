1c4c263a636da8800cd813ed50cc9e9ba7f53ffe
Sebazt <juan.londono29@udea.edu.co>
2022-05-08 08:37:26 -0500

    add Technologies

--- js/skills.js
+++ js/skills.js
@@ -1,79 +1,89 @@
 var skillsArray =
 
-[
-  {
-    "id": 1,
-    "image": "../assets/html.png",
-    "name": "Html5"
-  },
-  {
-    "id": 2,
-    "image": "../assets/css.png",
-    "name": "Css3"
-  },
-  {
-    "id": 3,
-    "image": "../assets/js.png",
-    "name": "JavaScript"
-  },
-  {
-    "id": 4,
-    "image": "../assets/bootstrap.png",
-    "name": "Bootstrap"
-  },
-  {
-    "id": 5,
-    "image": "../assets/git.png",
-    "name": "Git"
-  },
-  {
-    "id": 6,
-    "image": "../assets/github.png",
-    "name": "Git-hub"
-  },
-  {
-    "id": 7,
-    "image": "../assets/react.png",
-    "name": "React"
-  },
-  {
-    "id": 8,
-    "image": "../assets/node.png",
-    "name": "Node.js"
-  },
-  {
-    "id": 9,
-    "image": "https://www.servicepilot.com/images/integration/mongodb.webp",
-    "name": "MongoDB"
-  },
-  {
-    "id": 10,
-    "image": "https://nashvillesoftwareschool.com/images/technologies/express.png",
-    "name": "Express"
-  },
-  {
-    "id": 11,
-    "image": "../assets/redux.png",
-    "name": "Redux"
-  },
-  {
-    "id": 12,
-    "image": "https://raw.githubusercontent.com/kristerkari/react-native-svg-transformer/master/images/react-native-logo.png",
-    "name": "React-Native"
-  },
-  {
-    "id": 13,
-    "image": "https://webintegral.com.co/wp-content/uploads/2018/05/postman-icon.png  ",
-    "name": "Postman"
-  },
-  {
-    "id": 14,
-    "image": "../assets/insomia.png",
-    "name": "Insomia"
-  },
-  {
-    "id": 15,
-    "image": "https://cdn2.downdetector.com/static/uploads/logo/figma2.png",
-    "name": "Figma"
-  }
-]
+  [
+    {
+      "id": 1,
+      "image": "../assets/html.png",
+      "name": "Html5"
+    },
+    {
+      "id": 2,
+      "image": "../assets/css.png",
+      "name": "Css3"
+    },
+    {
+      "id": 3,
+      "image": "../assets/js.png",
+      "name": "JavaScript"
+    },
+    {
+      "id": 4,
+      "image": "../assets/bootstrap.png",
+      "name": "Bootstrap"
+    },
+    {
+      "id": 5,
+      "image": "../assets/git.png",
+      "name": "Git"
+    },
+    {
+      "id": 6,
+      "image": "../assets/github.png",
+      "name": "Git-hub"
+    },
+    {
+      "id": 7,
+      "image": "../assets/react.png",
+      "name": "React"
+    },
+    {
+      "id": 8,
+      "image": "../assets/node.png",
+      "name": "Node.js"
+    },
+    {
+      "id": 9,
+      "image": "https://www.servicepilot.com/images/integration/mongodb.webp",
+      "name": "MongoDB"
+    },
+    {
+      "id": 10,
+      "image": "https://nashvillesoftwareschool.com/images/technologies/express.png",
+      "name": "Express"
+    },
+    {
+      "id": 11,
+      "image": "../assets/redux.png",
+      "name": "Redux"
+    },
+    {
+      "id": 12,
+      "image": "https://raw.githubusercontent.com/kristerkari/react-native-svg-transformer/master/images/react-native-logo.png",
+      "name": "React-Native"
+    },
+    {
+      "id": 13,
+      "image": "https://webintegral.com.co/wp-content/uploads/2018/05/postman-icon.png  ",
+      "name": "Postman"
+    },
+    {
+      "id": 14,
+      "image": "../assets/insomia.png",
+      "name": "Insomia"
+    },
+    {
+      "id": 15,
+      "image": "https://cdn2.downdetector.com/static/uploads/logo/figma2.png",
+      "name": "Figma"
+    },
+    {
+      "id": 16,
+      "image": "https://cdn-icons-png.flaticon.com/512/919/919832.png",
+      "name": "Typescript"
+    },
+    {
+      "id": 17,
+      "image": "https://www.zencos.com/wp-content/uploads/2021/11/aws-logo.png",
+      "name": "AWS"
+    },
+  ]
